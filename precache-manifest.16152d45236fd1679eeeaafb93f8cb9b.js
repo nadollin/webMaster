e6ea114b87038ca41e204a40a95445e62d8a0a2d
Nado <lincharlie2@gmail.com>
2019-11-24 20:43:45 +0800

    fixed route and url

---
+++ precache-manifest.16152d45236fd1679eeeaafb93f8cb9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5358eee04f3978361fc4c2e3f66e78c8",
+    "url": "http//www.nado.tw/webMaster/index.html"
+  },
+  {
+    "revision": "14f739fa16afff7119ad",
+    "url": "http//www.nado.tw/webMaster/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "681bf4ada096ff9fa0bd",
+    "url": "http//www.nado.tw/webMaster/static/css/main.77275d60.chunk.css"
+  },
+  {
+    "revision": "14f739fa16afff7119ad",
+    "url": "http//www.nado.tw/webMaster/static/js/2.038e4b6a.chunk.js"
+  },
+  {
+    "revision": "681bf4ada096ff9fa0bd",
+    "url": "http//www.nado.tw/webMaster/static/js/main.372daacd.chunk.js"
+  },
+  {
+    "revision": "429e8198b97b552b9937",
+    "url": "http//www.nado.tw/webMaster/static/js/runtime-main.024a4123.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47cf7dfeeb2fabbf96d514bb428cdda3.js"
+  "http//www.nado.tw/webMaster/precache-manifest.16152d45236fd1679eeeaafb93f8cb9b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http//www.nado.tw/webMaster/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
