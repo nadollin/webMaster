bc5c3639780cb4dfdfdde134a42ebc4957be147b
Nado <lincharlie2@gmail.com>
2019-11-24 20:14:23 +0800

    first commit

---
+++ precache-manifest.47cf7dfeeb2fabbf96d514bb428cdda3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f13c8fed18edeaebea5d724762b895",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14f739fa16afff7119ad",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "1cca5523f754b31bfbab",
+    "url": "/static/css/main.77275d60.chunk.css"
+  },
+  {
+    "revision": "14f739fa16afff7119ad",
+    "url": "/static/js/2.038e4b6a.chunk.js"
+  },
+  {
+    "revision": "1cca5523f754b31bfbab",
+    "url": "/static/js/main.52cb1910.chunk.js"
+  },
+  {
+    "revision": "dd33854bb8399f58cccd",
+    "url": "/static/js/runtime-main.0ebe18bd.js"
+  }
+]);
